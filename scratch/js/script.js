a859b408bfb064d360883ca7ed3b0aab147bf63f
Adam Lamagna <adamjames1920@gmail.com>
2013-11-20 11:04:04 -0500

    updated sandbox

--- scratch/js/script.js
+++ scratch/js/script.js
@@ -1,9 +1,11 @@
 
 $(document).ready(function() {
 	$('.blog h1 a').mouseenter(function(){
-		$('.blog h1 a').fadeTo('slow', .4);
+		$('.blog h1 a').fadeTo('slow', .2);
 	});
 	$('.blog h1 a').mouseleave(function(){
 		$('.blog h1 a').fadeTo('slow', 1);
 	});
 });
+
+	
