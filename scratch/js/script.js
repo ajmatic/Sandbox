3fa680565bc17778e49b5befb09b3eb1ff236f84
Adam Lamagna <adamjames1920@gmail.com>
2013-12-08 09:56:40 -0500

    added a few things to my sandbox

--- scratch/js/script.js
+++ scratch/js/script.js
@@ -1,11 +1,54 @@
+//movement.js
+//global variables
+var sprite;
+var x, y;    //position variables
 
-$(document).ready(function() {
-	$('.blog h1 a').mouseenter(function(){
-		$('.blog h1 a').fadeTo('slow', .2);
-	});
-	$('.blog h1 a').mouseleave(function(){
-		$('.blog h1 a').fadeTo('slow', 1);
-	});
-});
+//constants
+var MIN_X = 15;
+var MAX_X = 365;
+var MIN_Y = 85;
+var MAX_Y = 435;
+      
+function init(){
+  sprite = document.getElementById("sprite");
+} // end init
+
+function moveSprite(dx, dy){
+  var surface = document.getElementById("surface");
+ 
+  x = parseInt(sprite.style.left);
+  y = parseInt(sprite.style.top);
+
+  x += dx;
+  y += dy;
+        
+  checkBounds();
+
+  // move ball to new position
+  sprite.style.left = x + "px";
+  sprite.style.top = y + "px";
+        
+  //describe position
+  var output = document.getElementById("output");
+  output.innerHTML = "x: " + x + ", y: " + y;
+} // end MoveSprite
+      
+function checkBounds(){
+  //wrap
+  if (x > MAX_X){
+    x = MIN_X;
+  } // end if
+  if (x < MIN_X){
+    x = MAX_X;
+  } // end if
+  if (y > MAX_Y){
+    y = MIN_Y;
+  } // end if
+  if (y < MIN_Y){
+    y = MAX_Y;
+  } // end if
+} // end function
+
+
 
 	
